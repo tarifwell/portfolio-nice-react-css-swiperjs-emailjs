739ec5ae2bfc7dbe4a8996d1af3a94c58da0563b
tarifwell <tarifwell@gmail.com>
2024-10-04 23:56:50 +0100

    feature: development of qualification (education and experience) and testimonials sections

---
+++ src/components/Testimonials/Data.jsx
@@ -0,0 +1,28 @@
+import img_person_1 from '../../assets/images_persons/img_testimonial_1.png';
+import img_person_2 from '../../assets/images_persons/img_testimonial_2.png';
+import img_person_3 from '../../assets/images_persons/img_testimonial_3.png';
+
+
+export const Data = [
+    {
+        id: 1,
+        name: 'Jane Doe',
+        position: 'Software Engineer',
+        image: img_person_1,
+        review: 'A really good job, all aspects of the project were followed step by step and with good results.'
+    },
+    {
+        id: 2,
+        name: 'Harry Clinton',
+        position: 'Software Engineer',
+        image: img_person_2,
+        review: 'I am extremely satisfied with the service provided by the team. They have been very responsive and professional. I am very happy with the results. I would recommend them to anyone.'
+    },
+    {
+        id: 3,
+        name: 'Sara Cill',
+        position: 'Software Engineer',
+        image: img_person_3,
+        review: 'A good work experience, I m satisfied with the results and the quality. I would recommend them to anyone.'
+    }
+]
--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,17 +1,61 @@
 import React from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
 
 import './Testimonials.css';
-import Data from './Data';
+import { Data } from './Data';
 
 const Testimonials = () => {
   return (
     <section className="testimonial container section" id="testimonial">
         <h2 className="section__title">Testimonials</h2>
-        <span className="section__subtitle">What My Clients Say</span>
+        <span className="section__subtitle">What My Clients Say</span>        
 
-        <div className="testimonial__container">{/* container grid */}
+        {/* <Swiper className="testimonial__container container grid"> */}
+        <Swiper className="testimonial__container"
+          
+          // slidesPerView={1}
+          loop={true}
+          grabCursor={true}
+          spaceBetween={24}
+          pagination={{
+            clickable: true,
+          }}
+          breakpoints={{
+            576: {
+              slidesPerView: 2,
+              // spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 48,
+            },
+            // 1024: {
+            //   slidesPerView: 5,
+            //   spaceBetween: 50,
+            // },
+          }}
+          modules={[Pagination]}
+        >
+          {Data.map(({id, name, image, review}, index) => {
+            return (
+              <SwiperSlide className="testimonial__card swiper-slide" key={`testimonial-${index}`}>
+                
+                <img src={image} alt="testimonial" className="testimonial__img" />
+                <h3 className="testimonial__name">{name}</h3>
+                <p className="testimonial__description">{review}</p>
+              </SwiperSlide>
+            )
+          })}
+        </Swiper>
 
-        </div>
         
     </section>
   )
